1f05b2362764b3efea821a91f612f4a85a9e0310
Trevor Kimenye <kimenye@gmail.com>
2012-08-07 18:40:42 +0300

    Added google vizualization api

--- public/javascripts/compare.js
+++ public/javascripts/compare.js
@@ -1,3 +1,28 @@
+var Unit = JS.Class({
+    construct:function (duration) {
+        this.duration = duration;
+        this.at = new Date();
+        this.cost = function() {
+            return 0;
+        };
+    }
+});
+
+var UwezoUnit = Unit.extend({
+    construct: function(duration) {
+        this.parent.construct.apply(this, [duration]);
+        this.startHour = 8;
+        this.endHour = 22;
+    }
+});
+
+var TuongeeUnit = Unit.extend({
+    construct: function(duration) {
+        this.parent.construct.apply(this, [duration]);
+        this.startHour = 6;
+        this.endHour = 18;
+    }
+});
 
 function Compare() {
     var self = this;
@@ -5,6 +30,11 @@ function Compare() {
     self.inProgess = ko.observable(false);
     self.seconds = ko.observable(0);
     self.timer = null;
+    self.uwezoUnits = ko.observableArray([]);
+    self.tuongeeUnits = ko.observableArray([]);
+    self.chartIsReady = ko.observable(false);
+    self.chart = null;
+    self.title = "Uwezo Tarriff vs Wakenya Tuongee";
 
     $('#stopwatch').click(function() {
         var _before = self.inProgess();
@@ -30,10 +60,13 @@ function Compare() {
     self.startAnimation = function() {
         self.setAnimationState(self.timers(), "running");
         self.timer = setInterval(self.tick, 1000);
+        self.drawChart();
     }
 
     self.tick = function() {
         self.seconds(self.seconds() + 1);
+        self.uwezoUnits.push(new UwezoUnit(self.seconds()));
+        self.uwezoUnits.push(new TuongeeUnit(self.seconds()));
     }
 
     self.pauseAnimation = function() {
@@ -58,6 +91,32 @@ function Compare() {
         item.css("-webkit-animation", "none");
         item.css("-o-animation", "none");
     }
+
+
+
+    self.drawChart = function() {
+        console.log("chart is ready ", self.chartIsReady());
+        if (self.chartIsReady()) {
+            this.chart = new google.visualization.LineChart(document.getElementById('timeline'));
+            var _options = { title: this.title };
+
+            var data = google.visualization.arrayToDataTable([
+                ['Time', 'Uwezo', 'Wakenya Tuongee']
+            ]);
+            this.chart.draw(data,options);
+        }
+    }
+
+    self.chartReady = function() {
+        console.log("Chart is ready");
+        self.chartIsReady(true);
+    }
+
+    self.initChart = function() {
+        google.load("visualization", "1", {packages:["corechart"]});
+        google.setOnLoadCallback(self.chartReady);
+    }
+//    self.initChart();
 
 }
 
