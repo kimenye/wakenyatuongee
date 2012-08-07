895e776e7ceebddacc0b6b5ec83eb82b7de4b809
Trevor Kimenye <kimenye@gmail.com>
2012-08-07 18:00:54 +0300

    Added a ticker to get the actual interval

--- public/javascripts/compare.js
+++ public/javascripts/compare.js
@@ -1,11 +1,14 @@
+
 function Compare() {
-//    $('#clock').stopwatch();
     var self = this;
 
     self.inProgess = ko.observable(false);
+    self.seconds = ko.observable(0);
+    self.timer = null;
 
     $('#stopwatch').click(function() {
         var _before = self.inProgess();
+        console.log("in click to " + !_before);
         self.inProgess(!_before);
         var _after = self.inProgess();
 
@@ -13,8 +16,12 @@ function Compare() {
             self.startAnimation();
         else
             self.pauseAnimation();
-
     });
+
+    $('#stopwatch').dblclick(function() {
+//        self.inProgess(false);
+//        self.stopAnimation();
+    })
 
     self.timers = function() {
         return $('.timer .numbers');
@@ -22,10 +29,20 @@ function Compare() {
 
     self.startAnimation = function() {
         self.setAnimationState(self.timers(), "running");
+        self.timer = setInterval(self.tick, 1000);
+    }
+
+    self.tick = function() {
+        self.seconds(self.seconds() + 1);
     }
 
     self.pauseAnimation = function() {
         self.setAnimationState(self.timers(), "paused");
+        clearInterval(self.timer);
+    }
+
+    self.stopAnimation = function() {
+        self.resetAnimation(self.timers(), "none");
     }
 
     self.setAnimationState = function(item, state) {
@@ -33,6 +50,13 @@ function Compare() {
         item.css("-moz-animation-play-state", state);
         item.css("-webkit-animation-play-state", state);
         item.css("-o-animation-play-state", state);
+    }
+
+    self.resetAnimation = function(item) {
+        item.css("animation", "none");
+        item.css("-moz-animation", "none");
+        item.css("-webkit-animation", "none");
+        item.css("-o-animation", "none");
     }
 
 }
