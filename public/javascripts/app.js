f79c49074181cdf8fd35458b8099bfab2ed646f0
Trevor Kimenye <kimenye@gmail.com>
2012-08-07 14:01:46 +0300

    Filtered rows appropriately

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,11 @@
 var Call = JS.Class({
     construct:function (json) {
         this.type = json["Call Type"];
-        this.callDate = json["Call Date"];
+        this.callDate = new Date(json["Call Date"]);
         this.duration = json.Duration;
         this.isOutGoing = this.type == "OUTGOING";
         this.isInComing = this.type == "INCOMING";
+        this.isNotEmpty = this.duration > 0;
     }
 });
 
@@ -15,36 +16,15 @@ function App() {
     this.mode = ko.observable(1);
 
     this.filteredCalls = ko.computed(function() {
-        var mode = this.mode();
-        console.log("Mode is " + mode);
-        var _before = this.calls();
-        console.log("Number of calls before is " + _before.length);
-//        return _.filter(_before, function(call) {
-//
-//            if (mode == 0) {
-//                console.log("Returning true because mode is " + mode);
-//                return true;
-//            }
-//            else if (mode == 1) {
-//                return call.isOutGoing == true;
-//            }
-//            else if (mode == 2) {
-//                return call.isInComing == true;
-//            }
-//        });
-        var _filtered = _.filter(_before, function(c) {
-           return false;
+        var _mode = this.mode();
+        return _.filter(this.calls(), function(call) {
+            return (_mode == 0 || (_mode == 1 && call.isOutGoing && call.isNotEmpty));
         });
     },this);
 
     this.toggle = function(value) {
 //        this.mode(value);
         console.log("Mode should be " + value);
-    };
-
-    self.selectView = function() {
-//        location.hash = view.type;
-        console.log("clicked!");
     };
 
     this.duration = ko.computed(function() {
